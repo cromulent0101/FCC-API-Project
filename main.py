5e6fa7a014a9805227b4e246912e7317c3b0f262
Joe Kaiser <kaiserfjoe@gmail.com>
2021-11-03 19:27:15 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "test"}
+
+@app.get("/posts")
+def get_posts():
+    return {"item_id": "this is your posts"}
