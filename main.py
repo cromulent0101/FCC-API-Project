6aabc6843f53faed479dfd3e66d43325ef078ea4
Joe Kaiser <kaiserfjoe@gmail.com>
2021-11-04 17:06:29 -0700

    added createposts and schema validation

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+# pylint: disable=no-name-in-module
+# pylint: disable=no-self-argument
+
 from typing import Optional
 from sys import displayhook
 from fastapi import FastAPI
+from fastapi import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def read_root():
@@ -12,4 +23,8 @@ def read_root():
 def get_posts():
     return {"item_id": "this is your posts"}
 
-print("committing from CLI worked, so trying from VSC to origin/main")
+@app.post("/createposts")
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"new_post": post}
