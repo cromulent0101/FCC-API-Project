b3a1cc7ada7911b8bc755c479f27c17275a40851
Joe Kaiser <kaiserfjoe@gmail.com>
2021-11-18 20:05:56 -0800

    Add ORM logic to GET all, GET by id, and POST

--- app/main.py
+++ app/main.py
@@ -9,21 +9,12 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-import models
-from database import engine, SessionLocal 
+from . import models
+from .database import engine,get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:   
-        db.close() 
-
-
 
 class Post(BaseModel):
     title: str
@@ -62,38 +53,46 @@ def read_root():
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
-    return {"status": "success"}
+    posts = db.query(models.Post).all()
+    return {"data": posts}
 
 
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+    posts = db.query(models.Post).all()
     return {"data": posts}
 
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts(title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title,post.content,post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts(title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title,post.content,post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
 
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"new_post": new_post}
 
 @app.get("/posts/{id}")
-def get_one_post(id: int,response: Response):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s""",str(id))
-    post = cursor.fetchone()
+def get_one_post(id: int, response: Response, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id==id).first()
+    print(post)
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s""",str(id))
+    # post = cursor.fetchone()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'post with id: {id} not found')
+                            detail=f'post with id: {idd} not found')
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'message':f'post with {id} not found'}
     return {"post_detail":post}
 
 @app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts where id = %s returning *""" % (str(id)),)
-    deleted_post = cursor.fetchone()
+def delete_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts where id = %s returning *""" % (str(id)),)
+    # deleted_post = cursor.fetchone()
     if not deleted_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'post with id: {id} not found')
@@ -102,9 +101,9 @@ def delete_post(id: int):
     return({"message": f"post {id} successfully rmemoved"})
 
 @app.put("/posts/{id}",status_code=status.HTTP_200_OK)
-def update_post(id: int,post: Post):
-    cursor.execute("""UPDATE posts SET title = \'%s\', content = \'%s\', published = %s WHERE id = %s RETURNING *""" % (post.title, post.content, post.published, str(id)),)
-    updated_post = cursor.fetchone()
+def update_post(id: int,post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""UPDATE posts SET title = \'%s\', content = \'%s\', published = %s WHERE id = %s RETURNING *""" % (post.title, post.content, post.published, str(id)),)
+    # updated_post = cursor.fetchone()
     if not updated_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'post with id: {id} not found')
