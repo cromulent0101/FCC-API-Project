336eb4e4ff5ba4c5c79989a289e76c6e75cbee61
Joe Kaiser <kaiserfjoe@gmail.com>
2021-11-27 10:56:12 -0800

    Add class for env variables

--- app/main.py
+++ app/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI,Response,status,HTTPException,Depends,APIRouter
 from fastapi import Body
 from sqlalchemy.orm import Session
 from . import models,schemas,utils
-from .database import engine,get_db
+from .database import engine
 from .routers import post,user,auth
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    database_pwd: str = "localhost"
+    database_username: str = "postgres"
+    sercet_key: str = "sakd;fjasd;kjasfdjasdfasdf"
+
+settings = Settings()
+
+print(settings.database_username)
 
 models.Base.metadata.create_all(bind=engine)
 
