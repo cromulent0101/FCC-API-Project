9687e31eb07764c5348d0ef14ef8b67c0be2ad51
Joe Kaiser <kaiserfjoe@gmail.com>
2021-11-29 20:56:03 -0800

    Return vote count when getting posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -4,6 +4,7 @@ from sys import displayhook
 from fastapi import FastAPI,Response,status,HTTPException,Depends,APIRouter
 from fastapi import Body
 import time
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from .. import models,schemas,utils,oauth2
 from ..database import engine,get_db
@@ -15,11 +16,15 @@ router = APIRouter(
     tags=['Posts']
 )
 
-@router.get("/",response_model=List[schemas.Post])  # need List of Posts because we return multiple
+@router.get("/",response_model=List[schemas.PostOut])  # need List of Posts because we return multiple
+# @router.get("/")  
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), 
     limit: int = 10, skip:int = 0, search: Optional[str] = ""):
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    print(limit)
+    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(
+        models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+
     return posts
 
 @router.post("/",status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
@@ -30,15 +35,18 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     db.refresh(new_post)
     return new_post
 
-@router.get("/{id}",response_model=schemas.Post)
+@router.get("/{id}",response_model=schemas.PostOut)
 def get_post(id: int, response: Response, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id==id).first()
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(
+        models.Post.id).filter(models.Post.id==id).first()  
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'post with id: {id} not found')
-    if post.user_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-            detail="that's not your post")
+    # print(post.user_id)
+    # if post.user_id != current_user.id:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+    #         detail="that's not your post")
     return post
 
 @router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)
@@ -61,6 +69,7 @@ def update_post(id: int,updated_post: schemas.PostBase, db: Session = Depends(ge
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'post with id: {id} not found')
+
     if post.user_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
             detail="that's not your post")
