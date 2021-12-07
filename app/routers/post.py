e90a795e5ce6b77ee2649193b76964491283db49
Joe Kaiser <kaiserfjoe@gmail.com>
2021-12-06 20:59:18 -0500

    Remove auth for getting all posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -18,8 +18,8 @@ router = APIRouter(
 
 @router.get("/",response_model=List[schemas.PostOut])  # need List of Posts because we return multiple
 # @router.get("/")  
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), 
-    limit: int = 10, skip:int = 0, search: Optional[str] = ""):
+def get_posts(db: Session = Depends(get_db), # , current_user: int = Depends(oauth2.get_current_user), 
+    limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
         models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(
