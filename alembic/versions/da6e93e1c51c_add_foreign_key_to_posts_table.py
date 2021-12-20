5b7f7ae9cab4087b2987528dd9b6325e4062c64b
Joe Kaiser <kaiserfjoe@gmail.com>
2021-12-20 11:52:23 -0500

    Fix alembic not adding votes table

--- alembic/versions/da6e93e1c51c_add_foreign_key_to_posts_table.py
+++ alembic/versions/da6e93e1c51c_add_foreign_key_to_posts_table.py
@@ -18,7 +18,6 @@ depends_on = None
 
 def upgrade():
     op.add_column('posts',sa.Column('owner_id',sa.Integer,nullable=False))
-    
     op.create_foreign_key('posts_users_fk', source_table='posts', referent_table='users',
                             local_cols=['owner_id'], remote_cols=['id'], ondelete="CASCADE")
     pass
