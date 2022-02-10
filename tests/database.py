b391fe901c2cb4b751430e57d6810ac5c1aebb50
Joe Kaiser <kaiserfjoe@gmail.com>
2022-02-10 07:23:52 -0500

    Split db test out

---
+++ tests/database.py
@@ -0,0 +1,46 @@
+from fastapi.testclient import TestClient
+import pytest
+from app.main import app
+from app import models
+from app.config import settings
+from app.database import get_db
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from alembic import command
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:12345@localhost:5432/fastapi-test'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@app.get("/")
+async def read_main():
+    return {"msg": "Hello World"}
+
+
+@pytest.fixture
+def client(session):
+    # run our code before we run our test
+    # command.upgrade("head",) # make sure you have the right test db env vars
+    def override_get_db():
+        try:
+            yield session
+        finally:   
+            session.close() 
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+    # run this code after our test finishes
+    #command.downgrade("base")
+ 
+@pytest.fixture
+def session():
+    models.Base.metadata.drop_all(bind=engine) # drop all tables to prevent duplicate users
+    models.Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:   
+        db.close() 
