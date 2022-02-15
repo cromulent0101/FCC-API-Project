fa210b77249ff2a615692cdc9a6b916665f30dfb
Joe Kaiser <kaiserfjoe@gmail.com>
2022-02-14 21:23:48 -0500

    Add oauth2 test using existing function

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,6 @@
 from app import schemas,models,oauth2
 from .database import client,session
+from jose import jwt
 from fastapi import HTTPException
 import pytest
 
@@ -28,9 +29,14 @@ def test_login_user(client,test_user):
     res = client.post("/login",data={"username": test_user['email'], "password": test_user['password']})
     login_res = schemas.Token(**res.json())
     try:
-        oauth2.verify_access_token(login_res.access_token,HTTPException)
+        token_data = oauth2.verify_access_token(login_res.access_token,HTTPException)
+        print('it worked')
     except HTTPException:
         print('bad token')
 
+    # payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    # id = payload.get('user_id')
     ## assert new_user.email=="ss@gmail.com"
+    assert token_data.id == test_user['id']
+    assert login_res.token_type=='bearer'
     assert res.status_code == 200
