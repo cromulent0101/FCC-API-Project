fe60e47349537ca81cabebbcdc0ebdbc6617b667
Joe Kaiser <kaiserfjoe@gmail.com>
2022-04-11 09:16:48 -0400

    Add remainder of post unit tests

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -77,3 +77,38 @@ def test_delete_post_not_exist(authorized_client,test_user,test_posts):
 def test_delete_other_user_post(authorized_client,test_user,test_posts):
     res = authorized_client.delete(f"/posts/{test_posts[3].id}")
     assert res.status_code == 403
+
+def test_update_post(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "new content",
+        "id": test_posts[0].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[0].id}",json=data)
+    updated_post = schemas.Post(**res.json())
+    assert res.status_code == 200
+    assert updated_post.title == data['title']
+    assert updated_post.content == data['content']
+
+def test_update_other_user_post(authorized_client, test_user, test_user2, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "new content",
+        "id": test_posts[3].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[3].id}",json=data)
+    assert res.status_code == 403
+
+def test_unauthorized_update_post(client,test_user,test_posts):
+    res = client.put(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+def test_update_post_not_exist(authorized_client,test_user,test_posts):
+    data = {
+        "title": "updated title",
+        "content": "new content",
+        "id": test_posts[3].id
+    }
+    
+    res = authorized_client.put("/posts/123123123",json=data)
+    assert res.status_code == 404
